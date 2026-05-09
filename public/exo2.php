403daa785a2936e8fd208fc89a2f42f6415231d5
anch365 <mchindaanchoura@gmail.com>
2026-05-09 13:06:26 +0200

    L'exercice 2 pour afficher la date courante sous forme de jj/mm/aa

--- public/exo2.php
+++ public/exo2.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercice 1</title>
+    <title>Exercice 2</title>
 </head>
 <body>
-    <h1>Afficher la date courante en respectant la forme jj/mm/aaaa :</h1>
+    <h1>Afficher la date courante en respectant la forme jj/mm/aa :</h1>
     <?php 
-    echo date('d/m/Y');
+    echo date('d-m-y');
     ?>
 </body>
 </html>
